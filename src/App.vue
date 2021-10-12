e697287588e829bfe70e293315773071f0239b16
zhuqinfei <1586859762@qq.com>
2021-10-12 23:50:04 +0800

    创建三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<template xmlns:router-link="http://www.w3.org/1999/html">
+  <div>
     <router-view/>
+    <hr>
+    <div>
+      <router-link to="/money">记账</router-link>
+      |
+      <router-link to="/labels">标签</router-link>
+      |
+      <router-link to="/statistics">统计</router-link>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-@import "~@/assets/style/test.scss";
-body{
-  background: $red
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
