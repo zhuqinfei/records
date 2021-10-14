e6d268699d6981cdc5b6cf317fd0d6369f2a1a9f
zhuqinfei <1586859762@qq.com>
2021-10-14 22:19:12 +0800

    新增标签高亮和修改meta viewport

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -35,11 +35,13 @@ export default {
      justify-content: center;
      align-items:center;
      flex-direction: column;
-
      .icon {
        width:30px;
        height:30px;
      }
+   }
+   > .item.selected{
+     color:red;
    }
  }
 </style>
