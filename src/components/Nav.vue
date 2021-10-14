358d4781dd3aba0dd04da323e03cec634e6244c8
zhuqinfei <1586859762@qq.com>
2021-10-14 14:19:01 +0800

    添加SVG的图标

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"/>
+      </svg>
+      记账
+    </router-link>
       |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      标签
+    </router-link>
       |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+let importAll=(requireContext:__WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
+try{
+  importAll(require.context('../assets/icons',true,/\.svg$/));
+}catch (error) {
+  console.log(error)
+}
+
 export default {
   name: 'Nav'
 };
