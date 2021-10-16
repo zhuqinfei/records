751d0360f629cacccd7b64fb2e788fb4913c9cdd
zhuqinfei <1586859762@qq.com>
2021-10-16 17:41:01 +0800

    添加money的css样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -26,7 +26,7 @@ export default {
 @import "~@/assets/styles/helper.scss";
  nav{
    display:flex;
-   box-shadow:0 0 3px rgba(0,0,0,0.25);
+   @extend %outerShadow;
    flex-direction: row;
    font-size: 12px;
    > .item{
