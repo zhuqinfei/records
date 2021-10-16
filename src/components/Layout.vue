753dcd6a9a048fd7190d12f1da5c4d45df424fc8
zhuqinfei <1586859762@qq.com>
2021-10-17 03:31:35 +0800

    基本完成Numberpad 组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="layout-wrapper">
+    <div class="content" :class="classPrefix && `${classPrefix}-content` ">
+<!--slot插槽，用户传入什么就是什么-->
+      <slot/>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props:['classPrefix'],
+  name: 'Layout'
+};
+</script>
+
+<style lang="scss">
+.layout-content{
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+<style lang="scss" scoped>
+.layout-wrapper{
+  display:flex;
+  flex-direction:column;
+  height:100vh;
+}
+.content{
+  flex-grow: 1;
+  overflow:auto;
+}
+</style>
