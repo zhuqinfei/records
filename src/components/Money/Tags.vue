8e10c0bf117dfcc1f1e121ecc0f3c17d26f5114d
zhuqinfei <1586859762@qq.com>
2021-10-17 18:03:59 +0800

    可以选择或取消tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,34 +4,31 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected:selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{tag}}</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
-};
+import Vue from 'vue';
+import {Component,Prop} from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue{
+ @Prop() dataSource:string[] | undefined  //string[]表示只能是字符串数组
+  selectedTags:string[]=[];
+
+ toggle(tag:string){
+    const index=this.selectedTags.indexOf(tag)
+   if(index>=0){
+     this.selectedTags.splice(index,1)
+   }else{
+     this.selectedTags.push(tag)
+   }
+ }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -52,6 +49,11 @@ export default {
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      $bg:#d9d9d9;
+      &.selected{
+        background:darken($bg,50%);
+        color:white;
+      }
     }
   }
   > .new{
