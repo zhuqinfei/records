9e182ce963ed83f4d5dc2a85cd0c31e10a616156
zhuqinfei <1586859762@qq.com>
2021-10-18 02:10:50 +0800

    添加 createAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,11 +16,14 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component,Watch} from 'vue-property-decorator';
 
+const recordList:Record[]=JSON.parse(window.localStorage.getItem('recordList')||'[]')
+
 type Record={  //如何在TS中声明一个复杂类型
   tags:string[];
   notes:string;
   type:string;
   amount:number;
+  createdAt:Date | undefined
 }
 
 @Component(
@@ -28,7 +31,7 @@ type Record={  //如何在TS中声明一个复杂类型
 )
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  recordList:Record[]=[];
+  recordList:Record[]=recordList
   record:Record={
     tags:[],notes:'',type:'-',amount:0
   };
@@ -45,9 +48,9 @@ export default class Money extends Vue {
     this.record.amount=parseFloat(value);
   };
   saveRecord(){
-    const record2=JSON.parse(JSON.stringify(this.record)) //深拷贝，经过两次转换变成两个不同地址,但是内容完成一样
+    const record2:Record=JSON.parse(JSON.stringify(this.record)) //深拷贝，经过两次转换变成两个不同地址,但是内容完成一样
+    record2.createdAt=new Date()
     this.recordList.push(record2)
-    console.log(this.recordList)
   }
   @Watch('recordList')
     onRecordListChanged(){
