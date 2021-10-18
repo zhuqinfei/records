8674394894d16d3d74afd4863023b2fd69c3d2b7
zhuqinfei <1586859762@qq.com>
2021-10-18 22:15:18 +0800

    将tag的string改成{id,name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,11 @@
 const localStorageKeyName = 'tagList';
+type Tag={
+  id:string
+  name:string
+}
 type TagListMode={
-  data:string[]    //data做到自己维护
-  fetch:()=>string[]
+  data:Tag[]    //data做到自己维护
+  fetch:()=>Tag[]
   create:(name:string)=>'success' | 'duplicated' //联合类型   //success表示成功，duplicated表示name重复
   save:()=>void
 }
@@ -12,8 +16,11 @@ const tagListModel:TagListMode = {
     return this.data
   },
   create(name:string){
-    if(this.data.indexOf(name)>=0){return 'duplicated'}
-    this.data.push(name)
+    //this.data=[{id='1',name='1'},{id='2',name='2'}] 长这样的
+    //通过map()收集所有name,产生一个新的数组
+    const names=this.data.map(item=>item.name)
+    if(names.indexOf(name)>=0){return 'duplicated'}
+    this.data.push({id:name,name:name})
     this.save();
     return 'success'
   },
