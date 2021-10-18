c013883b5873575bd7b56c9cba827217732153d6
zhuqinfei <1586859762@qq.com>
2021-10-18 17:20:00 +0800

    添加Labels组件HTML和css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,27 @@
 <template>
  <div>
    <Layout>
-     Labels.vue
+     <ol class="tags">
+       <li>
+         <span>衣</span>
+         <Icon name="right"/>
+       </li>
+       <li>
+         <span>食</span>
+         <Icon name="right"/>
+       </li>
+       <li>
+         <span>住</span>
+         <Icon name="right"/>
+       </li>
+       <li>
+         <span>行</span>
+         <Icon name="right"/>
+       </li>
+     </ol>
+     <div class="createTag-wrapper">
+       <button class="createTag">新增标签</button>
+     </div>
    </Layout>
  </div>
 </template>
@@ -13,5 +33,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding:0 16px;
+  &-wrapper {
+    text-align: center;
+    padding:16px;
+    margin-top:44-16px;
+  }
+}
 </style>
