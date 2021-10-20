1e07a58e53df6fed2a098821cc7380247f0fcee7
zhuqinfei <1586859762@qq.com>
2021-10-21 02:07:50 +0800

    封装 tagList 的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -21,4 +21,7 @@ type TagListModel={
 interface Window{
   tagList:Tag[],
   createTag:(name:string)=>void
+  removeTag:(id:string)=>boolean
+  updateTag:(id:string,name:string)=>'success' | 'not found' | 'duplicated'
+  findTag:(id:string)=>Tag
 }
