0bfda7d55f6a75d5776c0a2d8a8873ed93ed88ec
zhuqinfei <1586859762@qq.com>
2021-10-21 01:39:13 +0800

    封装window.createTag

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,20 @@ type RecordItem = {
   amount: number // 数据类型 object | string
   createdAt?: Date  // 类 / 构造函数
 }
+type Tag={
+  id:string
+  name:string
+}
+type TagListModel={
+  data:Tag[]    //data做到自己维护
+  fetch:()=>Tag[]
+  create:(name:string)=>'success' | 'duplicated' //联合类型   //success表示成功，duplicated表示name重复
+  update:(id:string,name:string)=>'success' | 'not found' | 'duplicated'
+  remove:(id:string)=>boolean
+  save:()=>void
+}
+
+interface Window{
+  tagList:Tag[],
+  createTag:(name:string)=>void
+}
