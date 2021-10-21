25a7d9a78b4452b70413e371d6fb168d310e677e
zhuqinfei <1586859762@qq.com>
2021-10-21 18:07:46 +0800

    完成模块化store

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,21 +1,21 @@
 import clone from '@/lib/clone';
 
 const localStorageKeyName = 'recordList';
-let data:RecordItem[] | undefined=undefined
+
 
 const recordStore={
+  recordList:[] as RecordItem[],
   fetchRecords() {
-    data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-    return data;
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
   },
   saveRecords() {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
   },
-  recordList:data,
-  createRecord: (record: RecordItem) => {
+  createRecord(record: RecordItem){
     const record2: RecordItem = clone(record);//深拷贝，经过两次转换变成两个不同地址,但是内容完成一样
     record2.createdAt = new Date();
-    data && data.push(record2);
+    this.recordList && this.recordList.push(record2);
     recordStore.saveRecords();
   },
 }
