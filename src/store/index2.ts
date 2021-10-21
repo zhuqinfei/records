eb599a78b41f732eaa79f3e4983482453a3785b1
zhuqinfei <1586859762@qq.com>
2021-10-21 17:09:05 +0800

    将store分成recordStore.ts和tagStore.ts

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,32 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store={
-  //record store
-  recordList:recordListModel.fetch(),
-  createRecord:(record:RecordItem)=>{
-    recordListModel.create(record)
-  },
-
-//tag store
-  tagList:tagListModel.fetch(),
-  findTag(id:string){   //这里不能用箭头函数，不然this就是指向外面的this
-    return this.tagList.filter (t=>t.id===id)[0]
-  },
-  createTag:(name:string)=>{
-    const message=tagListModel.create(name)
-    if(message==='duplicated'){
-      window.alert('标签重复了')
-    }else if(message==='success'){
-      window.alert('添加成功')
-    }
-  },
-  removeTag:(id:string)=>{
-    return tagListModel.remove(id);
-  },
-  updateTag:(id:string,name:string)=>{
-    return tagListModel.update(id,name)
-  }
+  ...recordStore,
+  ...tagStore
 }
 
 export default  store;
---
+++ src/store/recordStore.ts
@@ -0,0 +1,9 @@
+import recordListModel from '@/models/recordListModel';
+
+
+export  default {
+  recordList:recordListModel.fetch(),
+  createRecord:(record:RecordItem)=>{
+    recordListModel.create(record)
+  },
+}
