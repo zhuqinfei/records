57044e6f77525eabad1a3bd0d901591a31a1ec58
zhuqinfei <1586859762@qq.com>
2021-10-21 17:33:07 +0800

    将recordListModel融入store

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,9 +1,25 @@
-import recordListModel from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
+const localStorageKeyName = 'recordList';
+let data:RecordItem[] | undefined=undefined
 
-export  default {
-  recordList:recordListModel.fetch(),
-  createRecord:(record:RecordItem)=>{
-    recordListModel.create(record)
+const recordStore={
+  fetchRecords() {
+    data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return data;
+  },
+  saveRecords() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  },
+  recordList:data,
+  createRecord: (record: RecordItem) => {
+    const record2: RecordItem = clone(record);//深拷贝，经过两次转换变成两个不同地址,但是内容完成一样
+    record2.createdAt = new Date();
+    data && data.push(record2);
+    recordStore.saveRecords();
   },
 }
+
+recordStore.fetchRecords()
+
+export  default recordStore;
